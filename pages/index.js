a1524c646c40c883c408f498cadfe0411b151dfd
Leonardo Luize <leonardo.luize2@gmail.com>
2023-07-25 17:19:48 +0000

    feat: adiciona página "em construção"

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        flexDirection: "column",
+        marginTop: "10px",
+      }}
+    >
+      <h1
+        style={{
+          fontFamily: "sans-serif",
+          fontWeight: "normal",
+        }}
+      >
+        <strong>Recipe</strong>Share
+      </h1>
+      <h2
+        style={{
+          fontFamily: "sans-serif",
+          fontWeight: "normal",
+          fontSize: "20px"
+        }}
+      >
+        Em construção... 🚧 
+      </h2>
+    </div>
+  );
 }
