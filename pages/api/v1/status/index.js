90ad0b5062433be2939ffce5a8bcbea83b2a6422
Leonardo Luize <leonardo.luize2@gmail.com>
2023-11-16 14:39:36 +0000

    feat: adiciona banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,9 @@
-export default function status(req, res) {
-  res.status(200).json({message: "rodando recipe share"});
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
+  res.status(200).json({
+    message: "rodando recipe share",
+  });
 }
