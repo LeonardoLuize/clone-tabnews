400cd063ffa9048369d45ce4e5dbe634d8cb787e
Leonardo Luize <leonardo.luize2@gmail.com>
2024-03-07 10:58:39 -0300

    feat: adiciona teste para rota de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const oppenedConnectionsResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';"
+  );
+
   res.status(200).json({
-    message: "rodando recipe share",
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        currentConnections: oppenedConnectionsResult.rowCount,
+      },
+    },
   });
 }
