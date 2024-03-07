9311fabeabbe366f40b5ad7033ccdbe6deeb4eaf
Leonardo Luize <leonardo.luize2@gmail.com>
2024-03-07 11:25:45 -0300

    feat: adiciona propriedade maxConnections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,16 +4,18 @@ export default async function status(req, res) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
   const oppenedConnectionsResult = await database.query(
     "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';"
   );
 
   res.status(200).json({
     updated_at: updatedAt,
-    dependecies: {
+    dependencies: {
       database: {
         version: databaseVersion.rows[0].server_version,
         currentConnections: oppenedConnectionsResult.rowCount,
+        maxConnections: parseInt(maxConnections.rows[0].max_connections),
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,8 +6,11 @@ test("GET to /api/v1/status should return 200", async () => {
   const responseBody = await res.json();
   expect(responseBody.updated_at).toBeDefined();
 
+  expect(responseBody.dependencies.database.maxConnections).toEqual(100);
+
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.currentConnections).toEqual(1);
 });
